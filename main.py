8ed75dce91ff3737e58d2cc061781d18b4b4c018
Ramon Silva <kokusilva206@gmail.com>
2025-06-25 15:56:21 -0300

    env criado

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
+from dotenv import dotenv_values
 
-mongo_uri = "mongodb+srv://koku_api_py:<db_password>@cluster0.qdcmbwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongo_uri = "mongodb+srv://koku_api_py:Ko15311015ku@cluster0.qdcmbwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongo_client = MongoClient(mongo_uri) 
 
 database = mongo_client['database_test']
@@ -8,3 +9,8 @@ test_collection = database ['test_collection']
 
 person = {"name": "lucca de enzo", "age": 12}
 test_collection.insert_one(person)
+
+config = dotenv_values('.env')
+mongo_uri = config ['URI_MONGO_ATLAS']
+
+mongo_client = MongoClient(mongo_uri)
