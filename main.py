54676b9ca4d39042136b08ba6704a42f4c6d3c29
Ramon Silva <kokusilva206@gmail.com>
2025-06-25 15:41:01 -0300

    database

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from pymongo import MongoClient
 mongo_uri = "mongodb+srv://koku_api_py:<db_password>@cluster0.qdcmbwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongo_client = MongoClient(mongo_uri) 
 
+database = mongo_client['database_test']
+test_collection = database ['test_collection']
+
+person = {"name": "lucca de enzo", "age": 12}
+test_collection.insert_one(person)
